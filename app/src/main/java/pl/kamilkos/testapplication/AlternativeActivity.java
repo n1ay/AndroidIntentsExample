f9fcd63b93d75ad41c2a7b115c7d1cad9d3ec0eb
kamil <kammmilkos@gmail.com>
2017-11-11 13:23:19 +0100

    Przesył danych przez intencje

--- app/src/main/java/pl/kamilkos/testapplication/AlternativeActivity.java
+++ app/src/main/java/pl/kamilkos/testapplication/AlternativeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by kamil on 11.11.17.
@@ -17,6 +18,9 @@ public class AlternativeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alternative_layout);
+
+        TextView myTextView = findViewById(R.id.textView);
+        myTextView.setText(getIntent().getExtras().getCharSequence("label"));
 
     }
 }
--- app/src/main/java/pl/kamilkos/testapplication/MainActivity.java
+++ app/src/main/java/pl/kamilkos/testapplication/MainActivity.java
@@ -14,14 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AlternativeActivity.class);
-                startActivity(intent);
-            }
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), AlternativeActivity.class);
+            intent.putExtra("label", "new text");
+            startActivity(intent);
         });
     }
 }
