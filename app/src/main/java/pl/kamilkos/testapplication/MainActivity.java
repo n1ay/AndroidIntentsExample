ba8a6494ab68f5a028645280acd5ee6f5f6cff6f
kamil <kammmilkos@gmail.com>
2017-11-11 13:36:29 +0100

    Intencje niejawne - przykład

--- app/src/main/java/pl/kamilkos/testapplication/MainActivity.java
+++ app/src/main/java/pl/kamilkos/testapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package pl.kamilkos.testapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,9 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+        Button buttonAGH = findViewById(R.id.button2);
+
         button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), AlternativeActivity.class);
             intent.putExtra("label", "new text");
+            startActivity(intent);
+        });
+
+        buttonAGH.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.agh.edu.pl"));
             startActivity(intent);
         });
     }
