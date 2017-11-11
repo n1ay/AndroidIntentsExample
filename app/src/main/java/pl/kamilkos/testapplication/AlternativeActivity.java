aeb3522a269081bfea8e16c3e5a69f2229193877
kamil <kammmilkos@gmail.com>
2017-11-11 13:01:24 +0100

    Przykład działania intencji jawnych - start nowej aktywności

---
+++ app/src/main/java/pl/kamilkos/testapplication/AlternativeActivity.java
@@ -0,0 +1,22 @@
+package pl.kamilkos.testapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by kamil on 11.11.17.
+ */
+
+public class AlternativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alternative_layout);
+
+    }
+}
